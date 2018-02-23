8876b7de21624ecbef53a6a112ca59ee863ab7bc
Kelli Parker <iamkelliparker@gmail.com>
2018-02-23 16:26:31 -0500

    adding the archive index

---
+++ www/project/archive/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Daily News Archive Projects</title>
+	<link rel="icon" type="image/png" href="http://interactive.nydailynews.com/favicons.png">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<link rel="apple-touch-icon" href="">
+	<meta name="format-detection" content="telephone=no" />
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+
+	<!-- Titles -->
+	<meta property="og:title" content='Daily News Archive Projects' />
+	<meta name="twitter:title" content='Daily News Archive Projects' />
+
+	<!-- Descriptions -->
+	<meta name="description" content="A list of Daily News archive projects" />
+	<meta property="og:description" content="A list of Daily News archive projects" />
+	<meta name="twitter:description" content="A list of Daily News archive projects" />
+
+	<!-- KEYWORD -->
+	<meta name="keywords" content="interactive, news, archives" />
+
+	<!-- LINK -->
+	<link rel="canonical" href="http://interactive.nydailynews.com/archive/">
+	<meta property="og:url" content="http://interactive.nydailynews.com/archive/" />
+	<meta name="twitter:url" content="http://interactive.nydailynews.com/archive/" />
+
+	<!-- THUMBNAIL IMAGE-->
+	<meta property="og:image" content="http://interactive.nydailynews.com/archive/img/share.png" />
+	<meta name="twitter:image" content="http://interactive.nydailynews.com/archive/img/share.png" />
+	<meta name="twitter:image:alt" content="A description of the twitter image" />
+	<meta property="og:image:width" content="1024" />
+	<meta property="og:image:height" content="512" />
+
+	<!-- PARSELY -->
+	<script type="application/ld+json">
+		{
+			"@context": "http://schema.org",
+			"@type": "NewsArticle",
+			"headline": "Daily News Archive Projects",
+			"url": "http://interactive.nydailynews.com/archive/",
+			"thumbnailUrl": "http://interactive.nydailynews.com/archive/img/share.png",
+			"dateCreated": "2018-02-23T06:00:00Z",
+			"articleSection": "Interactive",
+			"creator": ["Interactive Project"],
+			"keywords": ["interactive project","interactive","index"]
+		}
+	</script>
+
+	<!-- NO NEED TO FILL -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:domain" content="http://interactive.nydailynews.com"/>
+	<meta name="twitter:site" content="@nydailynews">
+	<meta name="twitter:creator" content="@nydailynews">
+	<meta name="decorator" content="responsive" />
+	<meta name="nydn_section" content="NY Daily News" />
+	<meta name="msvalidate.01" content="02916AAC0DA8B068EFE01D721E03ED7E" />
+	<meta name="p:domain_verify" content="78efe4f5c9935744af497772f68a0ee7" />
+	<meta property="fb:app_id" content="107464888913" />
+	<meta property="fb:admins" content="1594068001" />
+	<meta property="fb:pages" content="268914272540" />
+	<meta property="og:type" content="article" />
+	<meta property="og:site_name" content="NY Daily News" />
+	<meta property="article:publisher" content="https://www.facebook.com/NYDailyNews/" />
+	<meta name="localeCountry" content="US"/>
+	<meta name="localeLanguage" content="en" />
+
+	<!-- ADOBE ANALYTICS -->
+	<script src="//assets.adobedtm.com/4fc527d6fda921c80e462d11a29deae2e4cf7514/satelliteLib-c91fdc6ac624c6cbcd50250f79786de339793801.js"></script>
+	<link rel="stylesheet" href="//www.nydailynews.com/nydn/c/rh.css">
+	<link rel="stylesheet" href="http://interactive.nydailynews.com/css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300,700|PT+Serif" rel="stylesheet">
+	<script data-main="//www.nydailynews.com/nydn/js/rh.js?r=20170405001" src="//www.nydailynews.com/nydn/js/require.js?r=2016LIST" defer></script>
+	  
+	<script>
+		var nydn = nydn || {
+			"section": "NYDailyNews",
+			"subsection": "news",
+			"template": "article",
+			"revision": "201609014009",
+			"bidder": { contains: function() {} },
+			"targetPath": document.location.pathname
+		};
+		var nydnDO = [ { 
+			'title':'xxxDaily News Archive Projectsxxx', 
+			'link':'http://interactive.nydailynews.com/archive/', 
+			'p_type':'interactive', 
+			'section':'interactive' 
+		}];
+	</script>
+	
+	<!-- ADS-START -->
+	<script onload="nydn_ads('one');" src="/includes/template/template.js"></script>
+	<!-- ADS-END -->
+
+	<script src="/js/jquery.min.js"></script>
+
+	<script>var nav_params = {section: 'news archive', url: 'http://interactive.nydailynews.com/archive/'};</script>
+	<script src="/library/vendor-nav/vendor-include.js" defer></script>
+</head>
+
+<body id="nydailynews" data-section="nydailynews" data-subsection="NY Daily News">
+
+<!-- SITEHEADER-START -->
+<header id="templateheader"></header>
+<!-- SITEHEADER-END -->
+
+<!-- CONTENT-START -->
+<main>
+	<article>
+	<h1 class="center">Daily News Archive Projects</h1>
+
+	<div class="center">
+        <ul class="ra-share" id="ra-share-top" style="margin: 20px 0;">
+            <li class="rt-share-f"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http://interactive.nydailynews.com/archive/">facebook</a> </li>
+            <li class="ra-share-t"> <a target="_blank" href="https://twitter.com/intent/tweet?text=The Daily News' list of news archive projects%20http://interactive.nydailynews.com/archive/">Tweet</a></li>
+            <li class="ra-share-e"> <a href="mailto:?subject=The Daily News' list of news archive projects: http://interactive.nydailynews.com/archive/">email</a> </li>
+        </ul>
+    </div>
+
+    <p class="center">An index of the interactive projects related to our archival news stories. See also: our <a href="/longform/"> longform articles</a>, our <a href="/quiz/">latest quizzes</a>, our <a href="/project/">special projects</a>, and our <a href="/poll/">latest polls</a>.</p>
+
+    <ul>
+    	<li></li>
+    </ul>
+
+<div class="ad center">
+	<div id='div-gpt-ad-1423507761396-1'>
+		<script>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1423507761396-1'); });</script>
+	</div>
+</div>
+
+	</article>
+</main>
+
+  <div class="ad center">
+	<div id='div-gpt-ad-1423507761396-3'>
+	  <script>
+		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1423507761396-3'); });
+	  </script>
+	</div>
+  </div>
+<!-- CONTENT-END -->
+
+<!-- FOOTER-START -->
+<footer id="templatefooter"></footer>
+<!-- FOOTER-END -->
+<div id="ra-bp">
+	  </div> <section id="rao">  <div id="rao-close"></div> <div id="rao-wrap"></div> </section>
+</div>
+<div id="r-scripts">
+	<div id="parsely-root" style="display: none">
+		<span id="parsely-cfg" data-parsely-site="nydailynews.com"></span>
+	</div>
+	<div class="r-ad">
+		<div id="div-gpt-ad-x100">
+		</div>
+	</div>
+</div>
+</body>
+</html>
